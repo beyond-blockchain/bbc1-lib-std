a0d6afd1dbd2ab1d462f149c1376398b4ffc9b71
Kenji Saito <ks91@sfc.wide.ad.jp>
2019-03-18 15:26:23 +0900

    Support for application constants.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ bbc1_classifiers = [
 
 setup(
     name='bbc1-lib-std',
-    version='0.12.1',
+    version='0.13',
     description='Standard library of Beyond Blockchain One',
     long_description=readme,
     url='https://github.com/beyond-blockchain/bbc1',
